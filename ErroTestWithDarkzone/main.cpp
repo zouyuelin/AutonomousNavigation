bc5eccea221bb339b40b243297f641fbefe85f71
yueyun <58660028+zouyuelin@users.noreply.github.com>
2021-01-14 23:33:33 +0800

    Update main.cpp

--- ErroTestWithDarkzone/main.cpp
+++ ErroTestWithDarkzone/main.cpp
@@ -1,10 +1,19 @@
-/*���˼·��
- * 1.��Ƹ�����Ϊimgthread���ӽ���Ϊpowerpmac��
- *   a.�ڽ��ս��̹��캯���У�this->moveToThread(this)��(QApplication::instance()->thread())
- * 2.����������ͬ����Ľ��̣�
- *   b.���ӵķ�ʽΪQt::DirectConnection�������ۺ�������ڴ����źŵ��߳�������
- * 3.���������߳�
- *   c.����exec()�������������¼��������������ѭ��run��������Ӧ�ÿ����������ַ�ʽ
+/*设计思路：
+ * 1.设计父进程为imgthread，子进程为powerpmac，
+ *   a.1在接收进程构造函数中，this->moveToThread(this)或(QApplication::instance()->thread())
+ *   a.2或者在父进程的构造函数中实现connect(this,&A::signal,other,&B::slot)和other->start
+ *   a.3传递自定义参数时，可以用qRegisterMetaType<Mat>("Mat")的形式注册
+ *   a.4利用Object实现的多线程，需要使用moveToThread函数将它放到线程中，例如：
+ *
+    father_Thread = new QThread();
+    child = new child_Thread();
+    child->moveToThread(father_Thread);
+    father_Thread->start();
+
+ * 2.设置两个相同级别的进程，子线程和子线程之间
+ *   b.连接的方式为Qt::DirectConnection，这样槽函数会放在传递信号的线程中运行
+ * 3.创建两个线程
+ *   c.启动exec()处理函数进行事件处理，但如果是循环run函数，则应该考虑上面两种方式
 */
 
 #include <iostream>
