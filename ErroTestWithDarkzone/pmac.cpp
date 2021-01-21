de7d61021c0f999b5402074deaaffff6b0e1b16f
yueyun <58660028+zouyuelin@users.noreply.github.com>
2021-01-21 20:35:32 +0800

    Add files via upload

---
+++ ErroTestWithDarkzone/pmac.cpp
@@ -0,0 +1,85 @@
+#include "pmac.h"
+pmac::pmac(QObject *parent):runstatus(true),autoControl(true),temp(cv::Mat::zeros(1,3,CV_64F))
+{
+    pmacConnect = std::shared_ptr<telnet>(new telnet);
+    pmacConnect->connect();
+
+    qDebug()<<"Pre-process completed\n";
+}
+
+pmac::~pmac()
+{
+    this->requestInterruption();
+    this->quit();
+    this->wait();
+}
+
+inline bool pmac::Send(QString message)
+{
+    return pmacConnect->telSendMess(message);
+}
+
+inline QString pmac::receive()
+{
+    return pmacConnect->reciveMess();
+}
+
+void pmac::AllStop()
+{
+    QMutexLocker locker(&m_lock);
+    autoControl = false;
+    runstatus = false;
+    this->terminate();
+    this->wait();
+}
+
+void pmac::GetThePosMessage(cv::Mat message)
+{
+    QMutexLocker locker(&m_lock);
+    for(int i=0;i<2;i++)
+        temp.at<double>(0,i)=message.at<double>(0,i);
+}
+
+void pmac::run()
+{
+    qDebug()<<"Pmac send start\n";
+    //开始发送消息
+    Send(QString("p8306=0"));
+        while(runstatus)
+        {
+                while(temp.at<double>(0,0)!=0 && autoControl)
+                {
+                    QMutexLocker locker(&m_lock);
+                    //赋值语句需要一条指令周期
+                    //p1000表示旋转，p1500表示弯曲，p2000表示进给，p2500表示旋转角象限
+                    QString temp_cmd[3]={"p8294=","p8295=","p8293="};
+                    //m_lock.lock();
+                    //下位机根据象限发送信息
+                    if (0<=temp.at<double>(0,0) && temp.at<double>(0,0)<0.25) {
+                        temp_cmd[2] += QString::number(1);
+                    }
+                    else if (0.25<=temp.at<double>(0,0) && temp.at<double>(0,0)<0.5) {
+                        temp_cmd[2] += QString::number(2);
+                    }
+                    else if (0.5<=temp.at<double>(0,0) && temp.at<double>(0,0)<0.75) {
+                        temp_cmd[2] += QString::number(3);
+                    }
+                    else if (0.75<=temp.at<double>(0,0) && temp.at<double>(0,0)<1) {
+                        temp_cmd[2] += QString::number(4);
+                    }
+
+                    for(int i=0;i<2;i++)
+                        temp_cmd[i] += QString::number(temp.at<double>(0,i));//在极短的指令周期内赋值给temp_cmd
+                    //m_lock.unlock();
+
+                    Send(temp_cmd[0]);
+                    usleep(5);
+
+                    Send(temp_cmd[1]);
+                    usleep(5);
+
+                    Send(temp_cmd[2]);
+                    usleep(5);
+                }
+        }
+}
---
+++ ErroTestWithDarkzone/telnet.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include "telnet.h"
+
+#define WILL  251
+#define WONT  252
+#define DO    253
+#define DONT  254
+#define IAC   255
+
+using namespace std;
+
+telnet::telnet():ConnectStatus(false)
+{
+    stou1 = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    //socket版本
+    WORD wVersion;
+    wVersion = MAKEWORD(2, 1);
+
+    //初始化套接字库
+    WSADATA wsaData;
+    WSAStartup(wVersion, &wsaData);
+
+    SOCKADDR_IN server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family=AF_INET;
+
+    //监听23端口
+    //server_addr.sin_addr.S_un.S_addr=INADDR_ANY;
+    server_addr.sin_port = htons(IPPORT_TELNET);//IPPORT_TELNET
+    server_addr.sin_addr.s_addr = inet_addr("192.168.0.110");
+    memset(server_addr.sin_zero, 0x00, 8);
+
+    if ((sock2 = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+        qDebug()<<QString::fromLocal8Bit("套接字创建失败！")<<endl;
+    if (::connect(sock2, (struct sockaddr *) &server_addr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR)
+        qDebug()<<QString::fromLocal8Bit("PMAC连接失败！")<<endl;
+    else
+    {
+        qDebug()<<QString::fromLocal8Bit("PMAC创建成功")<<endl;
+    }
+    Sleep(3000);
+
+    //与服务器协商
+    telnetnvt();
+}
+
+telnet::~telnet()
+{
+    //关闭套接字
+    closesocket(sock2);
+    WSACleanup();
+}
+
+void telnet::connect()
+{
+    Sleep(1000);
+    telSendMess("root");//root
+    Sleep(1000);
+    qDebug()<<"root\n";
+
+    telSendMess("deltatau");//deltatau
+    Sleep(1000);
+    qDebug()<<"deltatau\n";
+
+    //get into the shell;
+    telSendMess("gpascii");//gpascii
+    Sleep(1000);
+    qDebug()<<"gpascii\n";
+}
+
+//发送数据
+inline bool telnet::telSendMess(QString str)
+{
+    char pBuff[256]={0};
+    int nRet = str.length();
+    QByteArray ba = str.toLatin1();
+    strncpy(pBuff, ba.data(), nRet);
+
+    if(send(sock2,ba.data(),nRet,0)<0)
+    {
+        qDebug()<<QString::fromLocal8Bit("发送失败:")<<ba.length()<<endl;
+        emit connectedReady(false);
+        return false;
+    }
+    else if(!ConnectStatus)
+    {
+        ConnectStatus = true;
+        qDebug()<<QString::fromLocal8Bit("发送成功")<<endl;
+        emit connectedReady(true);
+    }
+    //cout<<ba.data()<<endl;
+    send(sock2, "\r\n", 1, 0);//这个控制字符必须要---
+    return true;
+}
+
+//接收数据
+QString telnet::reciveMess()
+{
+    char msg[1024] = {0};
+    memset(msg, 0, 1024);
+    int res = recv(sock2, msg, 1024, 0);
+    if (res == SOCKET_ERROR)
+    {
+        qDebug()<<"Receive Failed\n";
+        return nullptr;
+    }
+    if (res == 0)
+    {
+        Sleep(10);
+        qDebug()<<"Can't get the message!\n";
+    }
+
+    return QString(msg);
+}
+
+void telnet::telnetnvt()
+{
+    unsigned char strRecvBuf[3] = {0};
+    while(1)
+    {
+        //选项协商需要 3 个字节
+        //例如 IAC DONT ECHO
+        //第一个 IAC 一个字节 (0xff)
+        if(recv(sock2, (char*)(strRecvBuf), 1, 0)!=1)
+            return;
+
+        //如果不是IAC 选项
+        if(strRecvBuf[0] != IAC)
+            break;
+
+        //第二个字节是 WILL 、 DO 、 WONT 、 DON''T 四者之一
+        //最后一个 ID 字节指明激活或禁止选项。
+        if(recv(sock2, (char*)(strRecvBuf+1), 2, 0)!=2)//接收到两个字节，WILL和ID
+            return;
+
+        //查看第二个字节是什么，以选择相应的代码
+        switch(strRecvBuf[1])
+        {
+            case WILL:	//WILL
+                strRecvBuf[1] = DO;	//DO
+                break;
+
+            case WONT:	//WONT
+                strRecvBuf[1] = DONT;	//DONT
+                break;
+
+            case DO:	//DO
+            case DONT:	//DONT
+                strRecvBuf[1] = WONT;	//WONT
+                break;
+
+            default:
+                return;
+        }
+
+        //发送信息反馈
+        send(sock2, (char*)strRecvBuf, 3, 0);
+        //qDebug()<<strRecvBuf[1];
+    }
+}
